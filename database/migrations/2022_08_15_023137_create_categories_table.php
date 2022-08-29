28896d86b63900f893c67b5d1741217a132e48bb
Nadali Khalili <nadalikh@gmai.com>
2022-08-29 19:46:58 +0430

    cleaning codes

--- database/migrations/2022_08_15_023137_create_categories_table.php
+++ database/migrations/2022_08_15_023137_create_categories_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-//            $table->unsignedBigInteger('product_id');
-//            $table->foreign('product_id')->references('id')->on("products")->onDelete("cascade");
         });
     }
 
